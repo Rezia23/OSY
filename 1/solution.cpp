3e4437bea1d1555091042c23ecc6b5686cc2c21f
Terezie Hrubanova <terezie-h@seznam.cz>
2021-03-12 09:35:17 +0100

    start project

---
+++ 1/solution.cpp
@@ -0,0 +1,89 @@
+#ifndef __PROGTEST__
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cstdint>
+#include <climits>
+#include <cfloat>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <vector>
+#include <array>
+#include <iterator>
+#include <set>
+#include <list>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <memory>
+#include <functional>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <pthread.h>
+#include <semaphore.h>
+#include "progtest_solver.h"
+#include "sample_tester.h"
+
+using namespace std;
+#endif /* __PROGTEST__ */
+
+class CCargoPlanner {
+public:
+    static int SeqSolver(const vector<CCargo> &cargo,
+                         int maxWeight,
+                         int maxVolume,
+                         vector<CCargo> &load);
+
+    void Start(int sales,
+               int workers);
+
+    void Stop(void);
+
+    void Customer(ACustomer customer);
+
+    void Ship(AShip ship);
+};
+
+// TODO: CCargoPlanner implementation goes here
+//-------------------------------------------------------------------------------------------------
+#ifndef __PROGTEST__
+
+int main(void) {
+    CCargoPlanner test;
+    vector<AShipTest> ships;
+    vector<ACustomerTest> customers{make_shared<CCustomerTest>(), make_shared<CCustomerTest>()};
+
+    ships.push_back(g_TestExtra[0].PrepareTest("New York", customers));
+    ships.push_back(g_TestExtra[1].PrepareTest("Barcelona", customers));
+    ships.push_back(g_TestExtra[2].PrepareTest("Kobe", customers));
+    ships.push_back(g_TestExtra[8].PrepareTest("Perth", customers));
+    // add more ships here
+
+    for (auto x : customers)
+        test.Customer(x);
+
+    test.Start(3, 2);
+
+    for (auto x : ships)
+        test.Ship(x);
+
+    test.Stop();
+
+    for (auto x : ships)
+        cout << x->Destination() << ": " << (x->Validate() ? "ok" : "fail") << endl;
+    return 0;
+}
+
+#endif /* __PROGTEST__ */
