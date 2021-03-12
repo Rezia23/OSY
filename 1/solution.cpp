61980378934d84f03591010ea34cbcc09961857f
Terezie Hrubanova <terezie-h@seznam.cz>
2021-03-12 16:58:40 +0100

    passes one input

--- 1/solution.cpp
+++ 1/solution.cpp
@@ -41,6 +41,24 @@ using namespace std;
 
 class CCargoPlanner {
 public:
+    vector<ACustomer> m_Customers;
+    vector<AShip> m_Ships;
+    vector<thread> m_Sales;
+    vector<thread> m_Workers;
+    vector<pair<AShip, vector<CCargo>>> m_PossibleCargoShip;
+
+    sem_t semShipSales;
+    sem_t semSalesWorkers;
+
+    mutex mutShip;
+    mutex mutCargo;
+    bool stopSignal = false;
+
+    CCargoPlanner(){
+        sem_init(&semShipSales,0, 0);
+        sem_init(&semSalesWorkers,0, 0);
+    }
+
     static int SeqSolver(const vector<CCargo> &cargo,
                          int maxWeight,
                          int maxVolume,
@@ -54,7 +72,100 @@ public:
     void Customer(ACustomer customer);
 
     void Ship(AShip ship);
+    void SalesFunc();
+    void WorkersFunc();
 };
+void CCargoPlanner::Stop() {
+    stopSignal = true;
+    for(size_t i = 0; i<m_Sales.size();i++){
+        sem_post(&semShipSales);
+    }
+    for(size_t i = 0; i<m_Sales.size();i++){
+        m_Sales[i].join();
+    }
+    for(size_t i = 0; i<m_Workers.size();i++){
+        sem_post(&semSalesWorkers);
+    }
+    for(size_t i = 0; i<m_Workers.size();i++){
+        m_Workers[i].join();
+    }
+}
+
+void CCargoPlanner::SalesFunc(){
+    AShip myShip;
+    while(!stopSignal){
+    //todo -- maybe done
+        sem_wait(&semShipSales);
+        mutShip.lock();
+        if(m_Ships.empty()){
+            mutShip.unlock();
+            if(stopSignal){
+                break;
+            }else{
+                continue;
+            }
+        }else{
+            myShip = m_Ships.front();
+            mutShip.unlock();
+
+            vector<CCargo> tmpCargo;
+            vector<CCargo> cargo;
+            for(size_t i = 0; i<m_Customers.size();i++){
+                m_Customers[i]->Quote(myShip->Destination(),tmpCargo);
+                cargo.insert(cargo.end(), tmpCargo.begin(), tmpCargo.end());
+            }
+            mutCargo.lock();
+            m_PossibleCargoShip.emplace_back(myShip, cargo);
+            mutCargo.unlock();
+            sem_post(&semSalesWorkers);
+        }
+    }
+}
+void CCargoPlanner::WorkersFunc() {
+    //todo -- maybe done
+    pair<AShip, vector <CCargo>> shipAndCargo;
+    while(!stopSignal){
+        sem_wait(&semSalesWorkers);
+        mutCargo.lock();
+        if(m_PossibleCargoShip.empty()){
+            mutCargo.unlock();
+            break;
+        } else{
+            vector<CCargo> load;
+            shipAndCargo = m_PossibleCargoShip.front();
+            mutCargo.unlock();
+            SeqSolver(shipAndCargo.second, shipAndCargo.first->MaxWeight(), shipAndCargo.first->MaxVolume(), load );
+            shipAndCargo.first->Load(load);
+        }
+    }
+}
+
+
+void CCargoPlanner::Start(int sales, int workers) {
+    for(int i = 0; i<sales; i++){
+        m_Sales.emplace_back(&CCargoPlanner::SalesFunc, this);
+    }
+    for(int i = 0; i<workers;i++){
+        m_Workers.emplace_back(thread(&CCargoPlanner::WorkersFunc, this));
+    }
+}
+
+int CCargoPlanner::SeqSolver(const vector<CCargo> &cargo, int maxWeight, int maxVolume, vector<CCargo> &load) {
+    return ProgtestSolver(cargo, maxWeight, maxVolume, load);
+}
+
+void CCargoPlanner::Customer(ACustomer customer) {
+    m_Customers.push_back(customer);
+}
+
+void CCargoPlanner::Ship(AShip ship) {
+    mutShip.lock();
+    m_Ships.push_back(ship);
+    mutShip.unlock();
+    sem_post(&semShipSales);
+}
+
+
 
 // TODO: CCargoPlanner implementation goes here
 //-------------------------------------------------------------------------------------------------
